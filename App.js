d452101bf2e59a1dc6862261fb915cb16a1eee2a
Suresh-Gudi <125329112+Suresh-Gudi@users.noreply.github.com>
2023-02-14 13:11:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoListCreation from './components/TodoListCreation';
+
+function App() {
+  return (
+    <div className="To-do">
+      <TodoListCreation />
+    </div>
+  );
+}
+
+export default App;
